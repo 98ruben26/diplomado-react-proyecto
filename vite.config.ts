dd5718ff7fdc903ec3b22dd06f81adbf2c989923
Rub3n <rub3n9826@gmail.com>
2026-05-08 14:28:35 -0400

    Modificacion en axios client

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
+
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/diplomado-react-proyecto/', 
   plugins: [react()],
+  base: '/diplomado-react-proyecto/', // ESTA LÍNEA ES VITAL
 })
-
