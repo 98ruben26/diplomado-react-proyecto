6a65777bf3b0157ed394b2799289dff67540c829
Rub3n <rub3n9826@gmail.com>
2026-05-08 14:50:57 -0400

    Modificacion en axios client2

--- src/lib/axiosCliente.ts
+++ src/lib/axiosCliente.ts
@@ -3,6 +3,8 @@
 import axios from 'axios';
 import { env } from '../config/env';
 
+console.log("Conectando a la API en:", env.API_URL || 'https://taskdone-node.onrender.com/api');
+
 export const axiosClient = axios.create({
   // import.meta.env.PROD es una variable de Vite que es true cuando ejecutas 'npm run build'
 baseURL: env.API_URL || 'https://taskdone-node.onrender.com/api',
@@ -11,3 +13,5 @@ baseURL: env.API_URL || 'https://taskdone-node.onrender.com/api',
     'Content-Type': 'application/json',
   },
 });
+
+
