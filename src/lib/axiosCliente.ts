f881e336c87f4d8548b5588ad7d867b86e9fb2da
Rub3n <rub3n9826@gmail.com>
2026-05-08 11:47:04 -0400

    modificacion enlace backend2

--- src/lib/axiosCliente.ts
+++ src/lib/axiosCliente.ts
@@ -5,7 +5,7 @@ import { env } from '../config/env';
 
 export const axiosClient = axios.create({
   // import.meta.env.PROD es una variable de Vite que es true cuando ejecutas 'npm run build'
-baseURL: env.API_URL || 'http://localhost:3000/api',
+baseURL: env.API_URL || 'http://localhost:3000',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
