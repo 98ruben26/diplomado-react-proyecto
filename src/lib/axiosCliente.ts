64ef528aa8e353c92b968daadf770654cae21ecf
Rub3n <rub3n9826@gmail.com>
2026-05-08 11:55:04 -0400

    modificacion enlace backend4

--- src/lib/axiosCliente.ts
+++ src/lib/axiosCliente.ts
@@ -5,7 +5,7 @@ import { env } from '../config/env';
 
 export const axiosClient = axios.create({
   // import.meta.env.PROD es una variable de Vite que es true cuando ejecutas 'npm run build'
-baseURL: env.API_URL || ' https://taskdone-node.onrender.com',
+baseURL: env.API_URL || ' https://taskdone-node.onrender.com/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
